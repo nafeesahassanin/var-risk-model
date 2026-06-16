92ea574762b332cd56bf37beb1e7ef0f60779a1e
Nafeesa77 <nafeesahassanin@gmail.com>
2026-06-16 15:29:10 -0700

    Add Historical VaR

--- var_model.py
+++ var_model.py
@@ -24,3 +24,20 @@ print(f"Average daily return: {returns.mean().values[0]:.4f}")
 print(f"Standard deviation:{returns.std().values[0]:.4f}")
 print(f"Worst single day: {returns.min().values[0]:.4f}")
 print(f"Best single day: {returns.max().values[0]:.4f}")
+
+# Define confidence level and portfolio size
+confidence_level = 0.95
+portfolio_value = 10000
+
+# Find the 5th percential of historical returns
+historical_var = np.percentile(returns, 100 -  confidence_level * 100)
+
+historical_var_dollar = portfolio_value * abs(historical_var)
+
+# Print the results
+print("\n HISTORICAL VAR")
+print(f"Confidence Level: {confidence_level * 100:.0f}%")
+print(f"Portfolio Value: ${portfolio_value:,.0f}")
+print(f"Historical VaR (daily): {historical_var:.4f} ({historical_var * 100:.2f}%)")
+print(f"Historical VaR in dollars: ${historical_var_dollar:,.2f}")
+print(f"Interpretation: On 95% of days, losses won't exceed ${historical_var_dollar:,.2f}")
